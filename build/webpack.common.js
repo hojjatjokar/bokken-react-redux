28a8a70bed5f1c158cc63fc916cf6a051eca986c
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-10-29 16:17:40 +0330

    chore: configure react with ts

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.ts',
+    app: './src/index.tsx',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+const App = () => {
+  return <div>React Application</div>;
+};
+export default App;
