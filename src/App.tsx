d71b3ffbcde835f9c401bbb692aa263c784ecb53
Hojjat Jokar <7152729+hojjatjokar@users.noreply.github.com>
2021-10-31 19:40:01 +0330

    chore: configure redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import Counter from './features/counter/Counter';
+
 const App = () => {
-  return <div>React Application</div>;
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
 };
 export default App;
---
+++ src/features/counter/Counter.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { RootState } from '../../app/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
+
+function Counter() {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
